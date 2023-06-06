6fadc08c5cc0c20afc35f5786570a89b314f451e
zobweyt <zobweyt@gmail.com>
2023-06-07 00:25:13 +0300

    Refactoring

--- Template/Program.cs
+++ Template/Program.cs
@@ -3,46 +3,39 @@ using Discord.Addons.Hosting;
 using Discord.WebSocket;
 using Template.Services;
 
-namespace Template;
-
-internal class Program
-{
-    private static async Task Main()
+var host = Host.CreateDefaultBuilder()
+    .ConfigureDiscordHost((context, config) =>
     {
-        var host = Host.CreateDefaultBuilder()
-            .ConfigureDiscordHost((context, config) =>
-            {
-                config.SocketConfig = new DiscordSocketConfig
-                {
-                    LogLevel = LogSeverity.Debug,
-                    // Enable the setting below if you use GatewayIntents.GuildMembers
-                    AlwaysDownloadUsers = false,
-                    // The setting below allows application to get the reactions or content of a message.
-                    // Our template does not have such functionality, but that may be useful for you.
-                    MessageCacheSize = 0,
-                    GatewayIntents = GatewayIntents.AllUnprivileged // Configure more at https://discord.com/developers/applications.
-                };
+        config.SocketConfig = new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Debug,
+            // Enable the setting below if you use GatewayIntents.GuildMembers
+            AlwaysDownloadUsers = false,
+            // The setting below allows application to get the reactions or content of a message.
+            // Our template does not have such functionality, but that may be useful for you.
+            MessageCacheSize = 0,
+            GatewayIntents = GatewayIntents.AllUnprivileged, // Configure more at https://discord.com/developers/applications.
+            LogGatewayIntentWarnings = false
+        };
 
-                // Json files are not fully secured, so you may mistakenly commit them with token in your repository.
-                // You can find the full guide about this topic at https://github.com/zobweyt/Discord.NET-Template#advanced-configuration.
-                string? token = context.Configuration["Token"];
+        // Json files are not fully secured, so you may mistakenly commit them with token in your repository.
+        // You can find the full guide about this topic at https://github.com/zobweyt/Discord.NET-Template#advanced-configuration.
+        string? token = context.Configuration["Token"];
 
-                if (string.IsNullOrEmpty(token))
-                    throw new ArgumentNullException(nameof(token), "Token is null or empty. Specify it in your config.");
+        if (string.IsNullOrEmpty(token))
+            throw new ArgumentNullException(nameof(token), "Token is null or empty. Specify it in your config.");
 
-                config.Token = token;
-            })
-            .UseInteractionService((context, config) =>
-            {
-                config.LogLevel = LogSeverity.Debug;
-                config.UseCompiledLambda = true;
-            })
-            .ConfigureServices((context, services) =>
-            {
-                services.AddHostedService<InteractionHandler>();
-            })
-            .Build();
+        config.Token = token;
+    })
+    .UseInteractionService((context, config) =>
+    {
+        config.LogLevel = LogSeverity.Debug;
+        config.UseCompiledLambda = true;
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<InteractionHandlingService>();
+    })
+    .Build();
 
-        await host.RunAsync();
-    }
-}
+await host.RunAsync();
--- Template/Services/InteractionHandlingService.cs
+++ Template/Services/InteractionHandlingService.cs
@@ -7,14 +7,14 @@ using Discord.WebSocket;
 
 namespace Template.Services;
 
-internal sealed partial class InteractionHandler : DiscordClientService
+internal sealed partial class InteractionHandlingService : DiscordClientService
 {
     private readonly IServiceProvider _provider;
     private readonly InteractionService _service;
     private readonly IHostEnvironment _environment;
     private readonly IConfiguration _configuration;
 
-    public InteractionHandler(DiscordSocketClient client, ILogger<DiscordClientService> logger, IServiceProvider provider, InteractionService service, IHostEnvironment environment, IConfiguration configuration)
+    public InteractionHandlingService(DiscordSocketClient client, ILogger<DiscordClientService> logger, IServiceProvider provider, InteractionService service, IHostEnvironment environment, IConfiguration configuration)
         : base(client, logger)
     {
         _provider = provider;
@@ -39,17 +39,19 @@ internal sealed partial class InteractionHandler : DiscordClientService
 
     private async Task RegisterCommandsAsync()
     {
+        ulong devGuildId = _configuration.GetValue<ulong>("DevGuild");
+
         // If "DOTNET_ENVIRONMENT": "Development" commands will be registered to your development guild.
         if (_environment.IsDevelopment())
         {
             await Client.Rest.DeleteAllGlobalCommandsAsync();
-            await _service.RegisterCommandsToGuildAsync(_configuration.GetValue<ulong>("DevGuild"));
+            await _service.RegisterCommandsToGuildAsync(devGuildId);
             return;
         }
 
         // Otherwise commands will register globally.
         // Remember that all global operations take up to an hour.
-        await Client.Rest.BulkOverwriteGuildCommands(Array.Empty<ApplicationCommandProperties>(), _configuration.GetValue<ulong>("DevGuild"));
+        await Client.Rest.BulkOverwriteGuildCommands(Array.Empty<ApplicationCommandProperties>(), devGuildId);
         await _service.RegisterCommandsGloballyAsync();
     }
 
