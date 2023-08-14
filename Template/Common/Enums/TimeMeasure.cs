8b4e8e1d9809f3a154a215ef72f63bb38c88fbbb
zobweyt <zobweyt@gmail.com>
2023-08-15 02:11:25 +0300

    Optimize `RateLimitAttribute`

---
+++ Template/Common/Enums/TimeMeasure.cs
@@ -0,0 +1,22 @@
+namespace Template;
+
+/// <summary>
+/// Represents the time units.
+/// </summary>
+public enum TimeMeasure
+{
+    /// <summary>
+    /// Specifies the time in seconds.
+    /// </summary>
+    Seconds,
+
+    /// <summary>
+    /// Specifies the time in Minutes.
+    /// </summary>
+    Minutes,
+
+    /// <summary>
+    /// Specifies the time in Hours.
+    /// </summary>
+    Hours
+}
