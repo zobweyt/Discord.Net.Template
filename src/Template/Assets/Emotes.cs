297749bca4f268ae5542070d218eab1b9ea51029
zobweyt <zobweyt@gmail.com>
2024-04-13 14:27:22 +0300

    Update assets

---
+++ src/Template/Assets/Emotes.cs
@@ -0,0 +1,27 @@
+using Discord;
+
+namespace Template;
+
+// TODO: Adjust the emotes to fit your own.
+
+/// <summary>
+/// Represents a constant set of predefined <see cref="Emote"/> values.
+/// </summary>
+public static class Emotes
+{
+    /// <summary>
+    /// Represents a collection of brand logos.
+    /// </summary>
+    public static class Logos
+    {
+        /// <summary>
+        /// The official GitHub invertocat logo (https://github.com/logos).
+        /// </summary>
+        public static readonly Emote Github = "<:github:1131243046395183176>";
+
+        /// <summary>
+        /// The official Discord mark logo (https://discord.com/branding).
+        /// </summary>
+        public static readonly Emote Discord = "<:discord:1131243248409641000>";
+    }
+}
---
+++ src/Template/Assets/Icons.cs
@@ -0,0 +1,19 @@
+namespace Template;
+
+// TODO: Adjust the icons to fit your own.
+
+/// <summary>
+/// Represents a constant set of predefined icons.
+/// </summary>
+public static class Icons
+{
+    /// <summary>
+    /// The icon used to indicate a success state.
+    /// </summary>
+    public const string Check = "https://cdn.discordapp.com/emojis/1199976868057718876.webp?size=96&quality=lossless";
+
+    /// <summary>
+    /// The icon used to indicate an error state.
+    /// </summary>
+    public const string Cross = "https://cdn.discordapp.com/emojis/1199976870410715196.webp?size=96&quality=lossless";
+}
