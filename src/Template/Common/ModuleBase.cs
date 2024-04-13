1f507e028b08f85f8b933b210cd5d8a58bb02e8c
zobweyt <zobweyt@gmail.com>
2024-04-13 14:30:24 +0300

    Add TODO

---
+++ src/Template/Common/ModuleBase.cs
@@ -0,0 +1,11 @@
+using Discord.Interactions;
+
+namespace Template;
+
+/// <summary>
+/// An implementation of the <see cref="InteractionModuleBase"/> for this solution.
+/// </summary>
+public abstract class ModuleBase : InteractionModuleBase<SocketInteractionContext>
+{
+    // TODO: Define your common dependencies here.
+}
